0621d51d67e599fe1e2a48414006e004dd44bd0c
Tarrique <micuimoukiki@gmail.com>
2023-07-10 18:58:39 -0300

    Stylizando carrossel

--- src/componentes/Explicacao/index.js
+++ src/componentes/Explicacao/index.js
@@ -3,7 +3,8 @@ import { Carousel } from 'react-bootstrap';
 import './style.css';
 import Logo from "../img/lixos2.jpg";
 import Celular from "../img/pngwing.com.png";
-import Celular2 from "../img/celular2.avif";
+import Celular2 from "../img//Mao_Segurando_Celular_PNG_Sem_Fundo_Transparente.png";
+import Celular3 from "../img/celular3.png";
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -20,34 +21,35 @@ function Explicacao(props) {
       <h2>COMO FUNCIONA O CLEANMAP ?</h2>
       <Carousel>
         <Carousel.Item>
-        <div id='cxCelular'>
-          <img className='celular' src={Celular} alt="Celular" />
-          </div>          
+          <div id='cxCelular'>
+            <img className='celular' src={Celular} alt="Celular" />
+          </div>
           <Carousel.Caption>
             <div className='frases'>
-              <p>1º Abra o App no seu celular.</p>
+              <p>1º Abra o App no seu celular,<p>
+              </p> em seguida abra a câmera do App</p>
             </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <div id='cxCelular'>
-          <img className='celular' src={Celular} alt="Celular" />
+            <img className='celular' id='cel2' src={Celular2} alt="Celular" />
           </div>
           <Carousel.Caption>
             <div className='frases'>
-              <p>1º Abra o App no seu celular.</p>
+              <p>2º Bata uma foto com seu celular.</p>
             </div>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-        <div id='cxCelular'>
-          <img className='celular' src={Celular} alt="Celular" />
-          </div>          
+          <div id='cxCelular'>
+            <img className='celular' src={Celular3} alt="Celular" />
+          </div>
           <Carousel.Caption>
-            <div className='frases'>
-              <p>1º Abra o App no seu celular.</p>
+            <div className='frases' id='terceiraFrase'>
+              <p>3º Faça a postagem do resíduo!</p>
             </div>
-            <div className='botão'>
+            <div className='botao'>
               <button className="btnContinuar" onClick={handleClick} disabled={false} type="submit">Continuar</button>
             </div>
           </Carousel.Caption>
