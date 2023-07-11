b18f92cc2d3b52cf236fb1edd55e6e629491866d
Tarrique <micuimoukiki@gmail.com>
2023-07-11 16:08:14 -0300

    update no css

--- src/componentes/Camera/index.js
+++ src/componentes/Camera/index.js
@@ -74,6 +74,14 @@ function CameraPage() {
     inputElement.click();
   };
 
+  const removeImage = (index) => {
+    setCapturedImagesList((prevList) => {
+      const updatedList = [...prevList];
+      updatedList.splice(index, 1);
+      return updatedList;
+    });
+  };
+
   return (
     <div>
       <button className="btnVoltar" onClick={retornar} disabled={false} type="submit">
@@ -110,7 +118,12 @@ function CameraPage() {
           <Carousel>
             {capturedImagesList.map((image, index) => (
               <Carousel.Item key={index}>
-                <img className="d-block w-100" id='' src={image} alt={`Captured Image ${index + 1}`} />
+                <img className="d-block w-100" src={image} alt={`Captured Image ${index + 1}`} />
+                <button className="remove-button" onClick={() => removeImage(index)}>
+                  <svg id='iconDeletar' xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#4F5285" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                  </svg>
+                </button>
               </Carousel.Item>
             ))}
           </Carousel>
