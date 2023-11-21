f3958894ef12ea3ff08b6521c22b182a3dbf369c
Iuri <iuri-rdutra2@educar.rs.gov.br>
2023-11-21 08:39:18 -0300

    arrumação url

--- src/componentes/Inicio/index.js
+++ src/componentes/Inicio/index.js
@@ -25,7 +25,7 @@ function Inicio() {
     console.log("Chamando a função fetchPostagens");
     try {
       const response = await axios.get(
-        "http://localhost:3000/posts/list"
+        "https://mapeamentolixo.onrender.com/posts/list"
         //https://mapeamentolixo.onrender.com/posts/list
       );
       const postCoordinates = [];
@@ -109,7 +109,7 @@ function Inicio() {
 
     try {
       const response = await axios.put(
-        `http://localhost:3000/posts/${_id}/conclude`
+        `https://mapeamentolixo.onrender.com/posts/${_id}/conclude`
       );
 
       console.log(response.data);
